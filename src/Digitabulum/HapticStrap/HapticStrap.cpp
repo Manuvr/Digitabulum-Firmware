4e2b353b70022987f79c003f712c08f7f094e9b8
jspark311 <josh.lindsay@gmail.com>
2016-05-17 16:18:42 -0700

    Moving pin-assignments to their appropriate classes.

---
+++ src/Digitabulum/HapticStrap/HapticStrap.cpp
@@ -0,0 +1,156 @@
+/*
+File:   HapticStrap.cpp
+Author: J. Ian Lindsay
+Date:   2016.05.17
+
+Copyright 2016 Manuvr, Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
+
+#include "HapticStrap.h"
+#include <DataStructures/StringBuilder.h>
+
+#include "stm32f7xx_hal.h"
+
+
+
+// If your driver represents a statically-allocated unit, you can do something
+//   like this...
+//volatile HapticStrap* HapticStrap::INSTANCE = NULL;
+
+
+HapticStrap::HapticStrap() {
+  //INSTANCE = this;
+}
+
+
+HapticStrap::~HapticStrap() {
+}
+
+
+void HapticStrap::gpioSetup() {
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  // GPIO for haptic vibrators...
+  // PB10     ------> TIM2_CH3
+  // PB11     ------> TIM2_CH4
+  GPIO_InitStruct.Pin       = GPIO_PIN_10|GPIO_PIN_11;
+  GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull      = GPIO_NOPULL;
+  GPIO_InitStruct.Speed     = GPIO_SPEED_LOW;
+  GPIO_InitStruct.Alternate = GPIO_AF1_TIM2;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+}
+
+
+/****************************************************************************************************
+*  ▄▄▄▄▄▄▄▄▄▄▄  ▄               ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄        ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄
+* ▐░░░░░░░░░░░▌▐░▌             ▐░▌▐░░░░░░░░░░░▌▐░░▌      ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+* ▐░█▀▀▀▀▀▀▀▀▀  ▐░▌           ▐░▌ ▐░█▀▀▀▀▀▀▀▀▀ ▐░▌░▌     ▐░▌ ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀
+* ▐░▌            ▐░▌         ▐░▌  ▐░▌          ▐░▌▐░▌    ▐░▌     ▐░▌     ▐░▌
+* ▐░█▄▄▄▄▄▄▄▄▄    ▐░▌       ▐░▌   ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌ ▐░▌   ▐░▌     ▐░▌     ▐░█▄▄▄▄▄▄▄▄▄
+* ▐░░░░░░░░░░░▌    ▐░▌     ▐░▌    ▐░░░░░░░░░░░▌▐░▌  ▐░▌  ▐░▌     ▐░▌     ▐░░░░░░░░░░░▌
+* ▐░█▀▀▀▀▀▀▀▀▀      ▐░▌   ▐░▌     ▐░█▀▀▀▀▀▀▀▀▀ ▐░▌   ▐░▌ ▐░▌     ▐░▌      ▀▀▀▀▀▀▀▀▀█░▌
+* ▐░▌                ▐░▌ ▐░▌      ▐░▌          ▐░▌    ▐░▌▐░▌     ▐░▌               ▐░▌
+* ▐░█▄▄▄▄▄▄▄▄▄        ▐░▐░▌       ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌     ▐░▐░▌     ▐░▌      ▄▄▄▄▄▄▄▄▄█░▌
+* ▐░░░░░░░░░░░▌        ▐░▌        ▐░░░░░░░░░░░▌▐░▌      ▐░░▌     ▐░▌     ▐░░░░░░░░░░░▌
+*  ▀▀▀▀▀▀▀▀▀▀▀          ▀          ▀▀▀▀▀▀▀▀▀▀▀  ▀        ▀▀       ▀       ▀▀▀▀▀▀▀▀▀▀▀
+*
+* These are overrides from EventReceiver interface...
+****************************************************************************************************/
+
+/**
+* Debug support function.
+*
+* @return a pointer to a string constant.
+*/
+const char* HapticStrap::getReceiverName() {  return "HapticStrap";  }
+
+
+/**
+* Debug support function.
+*
+* @param A pointer to a StringBuffer object to receive the output.
+*/
+void HapticStrap::printDebug(StringBuilder* output) {
+  EventReceiver::printDebug(output);
+}
+
+
+
+/**
+* There is a NULL-check performed upstream for the scheduler member. So no need
+*   to do it again here.
+*
+* @return 0 on no action, 1 on action, -1 on failure.
+*/
+int8_t HapticStrap::bootComplete() {
+  EventReceiver::bootComplete();   // Call up to get scheduler ref and class init.
+  return 0;
+}
+
+
+
+/**
+* If we find ourselves in this fxn, it means an event that this class built (the argument)
+*   has been serviced and we are now getting the chance to see the results. The argument
+*   to this fxn will never be NULL.
+*
+* Depending on class implementations, we might choose to handle the completed Event differently. We
+*   might add values to event's Argument chain and return RECYCLE. We may also free() the event
+*   ourselves and return DROP. By default, we will return REAP to instruct the EventManager
+*   to either free() the event or return it to it's preallocate queue, as appropriate. If the event
+*   was crafted to not be in the heap in its own allocation, we will return DROP instead.
+*
+* @param  event  The event for which service has been completed.
+* @return A callback return code.
+*/
+int8_t HapticStrap::callback_proc(ManuvrRunnable *event) {
+  /* Setup the default return code. If the event was marked as mem_managed, we return a DROP code.
+     Otherwise, we will return a REAP code. Downstream of this assignment, we might choose differently. */
+  int8_t return_value = event->kernelShouldReap() ? EVENT_CALLBACK_RETURN_REAP : EVENT_CALLBACK_RETURN_DROP;
+
+  /* Some class-specific set of conditionals below this line. */
+  switch (event->event_code) {
+    default:
+      break;
+  }
+
+  return return_value;
+}
+
+
+
+int8_t HapticStrap::notify(ManuvrRunnable *active_event) {
+  int8_t return_value = 0;
+
+  switch (active_event->event_code) {
+    default:
+      return_value += EventReceiver::notify(active_event);
+      break;
+  }
+
+  if (local_log.length() > 0) {    Kernel::log(&local_log);  }
+  return return_value;
+}
+
+
+
+void HapticStrap::procDirectDebugInstruction(StringBuilder *input) {
+#ifdef __MANUVR_CONSOLE_SUPPORT
+#endif
+  if (local_log.length() > 0) {    Kernel::log(&local_log);  }
+}
